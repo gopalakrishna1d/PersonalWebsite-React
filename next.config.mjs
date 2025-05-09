4a9255d236322aac6481341f135c1c7b3f313f83
gopalakrishna711 <gopalkrishna.dangala@matdun.com>
2025-05-09 17:11:10 +0530

    git pages req changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.DEPLOY_TARGET === 'gh-pages';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+  basePath: isGithubPages ? '/<repo-name>' : '',
+  assetPrefix: isGithubPages ? '/<repo-name>/' : '',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
